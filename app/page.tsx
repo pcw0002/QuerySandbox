daf48931d13c09b3f13505fefe651ff42c051edf
Patrick Williams <pcw0002@auburn.edu>
2023-12-16 01:09:44 -0500

    - moar

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="mb-0 text-center lg:text-left">First Name, Last Name</p>
-      </div>
-
-      <div className="mb-32 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:text-left">
-        <div className="">
-          <h1 className="text-4xl font-bold mb-4">Hello, I&apos;m First Name</h1>
-          <p className="mb-8">
-            Head on over to the <a href="/users">users</a> page to see a list of users.
+        <p className="mb-8">
+            Head on over to the <a className="underline text-blue-400" href="/users">users</a> page to see a list of users.
           </p>
-          </div>
       </div>
     </main>
   )
